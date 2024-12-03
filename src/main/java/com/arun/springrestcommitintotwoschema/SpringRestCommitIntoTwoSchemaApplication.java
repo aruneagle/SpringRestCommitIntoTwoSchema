3e6ab7eee7b81ce7a07fc98e59c7e51f7f8e8ad3
aruneagle <arun.singh2early@gmail.com>
2024-12-03 15:48:31 -0500

    Initial skeleton

---
+++ src/main/java/com/arun/springrestcommitintotwoschema/SpringRestCommitIntoTwoSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.arun.springrestcommitintotwoschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCommitIntoTwoSchemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestCommitIntoTwoSchemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arun/springrestcommitintotwoschema/SpringRestCommitIntoTwoSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.springrestcommitintotwoschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCommitIntoTwoSchemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
