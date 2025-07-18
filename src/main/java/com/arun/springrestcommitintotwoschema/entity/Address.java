bc8f0295f90abcd82ff81df11ee90ff2ccffce8c
aruneagle <arun.singh2early@gmail.com>
2025-07-18 00:04:37 -0400

    add

--- src/main/java/com/arun/springrestcommitintotwoschema/entity/Address.java
+++ src/main/java/com/arun/springrestcommitintotwoschema/entity/Address.java
@@ -1,5 +1,28 @@
-package com.arun.springrestcommitintotwoschema.entity;/**
+package com.arun.springrestcommitintotwoschema.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedEntityGraph;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
  * @author arun on 12/3/24
  */
-    public class Address {
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String street;
+    private String city;
+    private String zipCode;
+    private String state;
+    private String country;
 }
--- src/main/java/com/arun/springrestcommitintotwoschema/entity/Customer.java
+++ src/main/java/com/arun/springrestcommitintotwoschema/entity/Customer.java
@@ -1,5 +1,35 @@
-package com.arun.springrestcommitintotwoschema.entity;/**
+package com.arun.springrestcommitintotwoschema.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+/**
  * @author arun on 12/3/24
  */
-    public class Customer {
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private Timestamp created;
+    private Timestamp modified;
+    private Timestamp deleted;
+    @OneToOne
+    private Address homeAddress;
+    @OneToOne
+    private Address officeAddress;
 }
